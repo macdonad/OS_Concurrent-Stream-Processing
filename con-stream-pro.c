3c5e53e92f8d587e4a8f65d695efbc43c55dbe21
macdonad <macdonad@mail.gvsu.edu>
2013-09-12 17:41:42 -0400

    Added Comments on Desc of Program

--- con-stream-pro.c
+++ con-stream-pro.c
@@ -12,9 +12,24 @@
 #include <stdio.h>
 #include <string.h>
 
+
+//Args
+// show the fist 'K' primes
+// Show all primes from 2 to a limit
 int
 main()
 {
   puts("Hello World");
   return 0;
 }
+
+
+//Parent gets list of all number in args from user
+
+//Parent finds first prime
+//Eliminates all numbers after that are a multiple of that first number
+//Passes new list to child process.
+
+//Terminate Gracefully
+
+//Use Pipes
