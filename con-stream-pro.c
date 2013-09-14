c64c7d937b0f62bc049c3c74db7d6ab0215460d5
macdonad <macdonad@mail.gvsu.edu>
2013-09-13 23:04:15 -0400

    Clean up clean up

--- con-stream-pro.c
+++ con-stream-pro.c
@@ -13,24 +13,27 @@
 #include <string.h>
 
 // GLOBALS
-int debug = 1;
+int debug = 0;
 int pid = 0;
+int mode;
+int limit = 0;
+int number = 0;
 
 //PROTOTYPES
 
+void prompt_user();
+void args_check(int, char*);
 
 
-
-//Args
-// show the fist 'K' primes
-// Show all primes from 2 to a limit
+//Prompt user for mode
+//user debug command line arg to see statuses
 int
 main(int argc, char *argv[])
-{
+{  
+  //Check args for "Debug"
+  //If in args, activate debug mode  
   int i = 0;
-  int mode;
-  int limit = 0;
-  int number = 0;
+  
   if(debug){printf("Total Number of Args: %d\n", argc);}
   while(i < argc)
     {
@@ -43,7 +46,25 @@ main(int argc, char *argv[])
       i++;
     }
   if(debug){printf("Debug = %d\n",debug);}
+
+  //Prompt User for Mode
+  prompt_user();
+
   
+
+
+
+
+
+
+  return 0;
+}
+
+//User Prompt
+//Get Mode and Limit or Number to generate
+void 
+prompt_user()
+{
   printf("Generate primes to a limit(1) or generate a number(0) of primes: ");
   scanf("%d", &mode);
   if(debug){printf("Mode: %d\n",mode);}
@@ -59,52 +80,4 @@ main(int argc, char *argv[])
       scanf("%d",&limit);
       if(debug){printf("Limit: %d\n", limit);}
     }
-
-
-
-  return 0;
 }
-
-
-//Parent gets list of all number in args from user
-
-//Parent finds first prime
-//Eliminates all numbers after that are a multiple of that first number
-//Passes new list to child process.
-void
-read_list()
-{
-  //Use this function to pass the list to the next child.
-  //if child read from pipe
-}
-
-void 
-write_list()
-{
-  //if parent write list to pipe
-}
-
-//Set up pipes with parent and child
-void 
-connect_pipes()
-{/*
-  if(pid < 0)
-    {
-      //error
-    }
-  else if (pid = 0)
-    {
-      //set up child's connection
-      read_list();
-    }
-  else if (pid > 0)
-    {
-      //set up Parent connection
-      write_list();
-      }*/
-}
-
-
-//Terminate Gracefully
-
-//Use Pipes
