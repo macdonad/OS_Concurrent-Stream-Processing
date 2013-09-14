c1190452ffae43367cc3c6d2b6adadc932bd159a
macdonad <macdonad@mail.gvsu.edu>
2013-09-14 00:52:44 -0400

    Functions for printing

--- con-stream-pro.c
+++ con-stream-pro.c
@@ -23,7 +23,11 @@ int number = 0;
 
 void prompt_user();
 void args_check(int, char*);
-
+void generate_number(int);
+void generate_limit(int);
+void print_header();
+void print_prime(int, int);
+void print_status(int, char*);
 
 //Prompt user for mode
 //user debug command line arg to see statuses
@@ -45,17 +49,10 @@ main(int argc, char *argv[])
 
       i++;
     }
-  if(debug){printf("Debug = %d\n",debug);}
+  if(debug){printf("Debug Activiated\n");}
 
   //Prompt User for Mode
   prompt_user();
-
-  
-
-
-
-
-
 
   return 0;
 }
@@ -66,18 +63,64 @@ void
 prompt_user()
 {
   printf("Generate primes to a limit(1) or generate a number(0) of primes: ");
-  scanf("%d", &mode);
-  if(debug){printf("Mode: %d\n",mode);}
+  scanf("%d", &mode);  
   if(mode == 0)
     {
+      if(debug){printf("Mode: Number\n");}
       printf("Enter number of primes to generate: "); 
       scanf("%d",&number);
       if(debug){printf("Number: %d\n", number);}
+      generate_number(number);
     }
   else if(mode == 1)
     {
+      if(debug){printf("Mode: Limit\n");}
       printf("Enter limit to generate primes to: ");
       scanf("%d",&limit);
       if(debug){printf("Limit: %d\n", limit);}
+      generate_limit(limit);
     }
+}
+
+//Generate specific number of primes
+void
+generate_number(int number)
+{
+  if(debug){printf("Number: %d\n", number);}
+  print_header();
+}
+
+//Generate primes to a limit
+void 
+generate_limit(int limit)
+{
+  if(debug){printf("Limit: %d\n", limit);}
+  print_header();
+  pid = getpid();
+  print_prime(pid, 2);
+  print_status(pid, "help");
+}
+
+//Print Header Info
+void 
+print_header()
+{
+  printf("PID  |  Prime #  |  Status Info\n");
+  printf("-------------------------------\n");
+}
+
+
+//Print PID and Prime Number
+void 
+print_prime(int pid, int number)
+{
+  printf("%d\t%d\n", pid, number);
+}
+
+//Print PID and Status
+//used in debugging
+void
+print_status(int pid, char* status)
+{
+  if(debug){printf("%d\t\t\t%s\n",pid, status);}
 }
